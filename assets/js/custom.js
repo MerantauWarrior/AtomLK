389771deb39ff8f8801a41b30fdea56e9f9f9bad
Boris <boris1488ss@gmail.com>
2021-03-26 19:59:09 +0200

    building main blocks 5

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,16 @@
 $(document).ready(function () {
   console.log("ready");
+
   // sidebar menu
   $('.js-sb-dd').click(function (e) {
     e.preventDefault();
     $(this).parent().toggleClass('sidebar-menu__list-item_active');
     $(this).siblings('.sidebar-submenu').toggle(250);
   });
+  // language
+  $('.header-lang__switch').click(function () {
+    $(this).toggleClass('header-lang__switch_active');
+    $('.header-lang__select').fadeToggle(250);
+  });
+
 });
